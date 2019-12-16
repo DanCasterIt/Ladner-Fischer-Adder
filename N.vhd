e398b5c5334b7c86c5a2c726228ee1c960c57871
Daniele Castro <danielecastro@hotmail.it>
2019-12-16 02:56:35 +0100

    added files

---
+++ N.vhd
@@ -0,0 +1,20 @@
+library ieee; 
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all;
+
+entity NETB is 
+		generic (
+			N : integer := 32
+		);
+		Port (
+			A: in std_logic_vector(N-1 downto 0);
+			B: in std_logic_vector(N-1 downto 0);
+			pO: out std_logic_vector(N-1 downto 0);
+			gO: out std_logic_vector(N-1 downto 0)
+		);
+end NETB;
+architecture BEHAVIORAL of NETB is
+begin
+	pO <= A xor B;
+	gO <= A and B;
+end BEHAVIORAL;
